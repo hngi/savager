f9f2af0d26893013b33466b35016b22b3425e527
jude okoroafor <judeokoroafor@gmail.com>
2020-07-30 07:21:16 +0100

    Added google analytics

--- rewards.php
+++ rewards.php
@@ -1,23 +1,32 @@
 <!doctype html>
 <html lang="en">
     <head>
+        <!-- Google Analytics -->
+        <script type="text/javascript" src="scripts/google-analytics.js"></script>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="title" content="Savager - rewards">
+        <meta name="description" content="Get rewards">
+        <meta name="author" content="Place-group-2">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="robots" content="index, follow">
+        <meta name="language" content="English">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Savager - rewards!</title>
+        <!-- Custom CSS -->
+        <link rel="stylesheet" type="text/css" href="style/rewards.css">
+        <title>Savager - rewards</title>
     </head>
     <body>
-
         <section class="navbar-section">
-
+            <?php  // require 'header.php'; ?>
         </section>
         <section class="hero-section">
 
         </section>
         <section class="footer-section">
-
+            <?php  // require 'footer.php'; ?>
         </section>
 
 
