ca525786bd1e6ef893d942d459dcaf23eac7c214
auleki <kodagiwa@gmail.com>
2020-07-30 15:56:42 +0100

    basic routing & controller

---
+++ controllers/indexController.js
@@ -0,0 +1,4 @@
+exports.viewTest = (req, res) => {
+    // res.render('index', { title: 'Express' });
+    res.send("Page Functional")
+}
---
+++ routes/indexRouter.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+const indexController = require("../controllers/indexController");
+
+router.get('/', indexController.viewTest);
+
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
