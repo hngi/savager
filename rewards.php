99e4b7a10dfa66974ad32a16f4020d8cc30b0b3c
jude okoroafor <judeokoroafor@gmail.com>
2020-07-30 18:33:57 +0100

    Localized files

--- rewards.php
+++ rewards.php
@@ -13,28 +13,169 @@
         <meta name="robots" content="index, follow">
         <meta name="language" content="English">
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style/vendor/bootstrap.min.css">
         <!-- Custom CSS -->
         <link rel="stylesheet" type="text/css" href="style/rewards.css">
         <title>Savager - rewards</title>
     </head>
     <body>
+        <!-- NACBAR SECTION -->
         <section class="navbar-section">
             <?php  // require 'header.php'; ?>
         </section>
+        <!-- HERO SECTION -->
         <section class="hero-section">
-
+            <div class="splash">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 mt-sm-5 offset-sm-1">
+                            <div class="hero-description pb-5 pt-5 text-left">
+                                <h2>Earned points:</h2>
+                                <p><span class="wallet">25</span> <sup>$</sup></p>
+                                <p>You've got a total of 500 points</p>
+                                <a class="btn btn-primary btn-lg text-uppercase" href="#" role="button">Withdraw</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-7">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
+        <!-- RECENT EARNING SECTION -->
+        <section class="recent-earning-section bg-color3">
+            <div class="recent-earning">
+                <div class="container p-3">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8 col-sm-12 ">
+                            <div class="title text-center mb-4">
+                                <h2>Recent rewards</h2>
+                            </div>
+                            <!-- FIRST POST -->
+                            <div class="row text-left meme-card pt-3 pb-3 mb-4">
+                                <div class="col-4">
+                                    <div class="preview-img">
+                                        <img src="img/meme1.jpg" alt="meme" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="preview-description">
+                                        <h4>Lorem Ipsum</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... <a href="#">see more</a></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="preview-reward">
+                                        <p></p>
+                                        <p><span class="wallet">125</span> <sup>$</sup></p>
+                                        <!-- <p>500 pts</p> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- SECOND POST -->
+                            <div class="row text-left meme-card pt-3 pb-3 mb-4">
+                                <div class="col-4">
+                                    <div class="preview-img">
+                                        <img src="img/meme1.jpg" alt="meme" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="preview-description">
+                                        <h4>Lorem Ipsum</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... <a href="#">see more</a></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="preview-reward">
+                                        <p></p>
+                                        <p><span class="wallet">5</span> <sup>$</sup></p>
+                                        <!-- <p>500 pts</p> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- THIRD POST -->
+                            <div class="row text-left meme-card pt-3 pb-3 mb-4">
+                                <div class="col-4">
+                                    <div class="preview-img">
+                                        <img src="img/meme1.jpg" alt="meme" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="preview-description">
+                                        <h4>Lorem Ipsum</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... <a href="#">see more</a></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="preview-reward">
+                                        <p></p>
+                                        <p><span class="wallet">25</span> <sup>$</sup></p>
+                                        <!-- <p>500 pts</p> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- THIRD POST -->
+                            <div class="row text-left meme-card pt-3 pb-3 mb-4">
+                                <div class="col-4">
+                                    <div class="preview-img">
+                                        <img src="img/meme1.jpg" alt="meme" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="preview-description">
+                                        <h4>Lorem Ipsum</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... <a href="#">see more</a></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="preview-reward">
+                                        <p></p>
+                                        <p><span class="wallet">212</span> <sup>$</sup></p>
+                                        <!-- <p>500 pts</p> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- THIRD POST -->
+                            <div class="row text-left meme-card pt-3 pb-3 mb-4">
+                                <div class="col-4">
+                                    <div class="preview-img">
+                                        <img src="img/meme1.jpg" alt="meme" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="preview-description">
+                                        <h4>Lorem Ipsum</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... <a href="#">see more</a></p>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="preview-reward">
+                                        <p></p>
+                                        <p><span class="wallet">20</span> <sup>pt</sup></p>
+                                        <!-- <p>500 pts</p> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- TEST SECTION -->
+        <!-- END TEST SECTION -->
+        <!-- FOOTER SECTION -->
         <section class="footer-section">
             <?php  // require 'footer.php'; ?>
         </section>
-
-
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="scripts/vendor/jquery-3.3.1.slim.min.js"></script>
+        <script src="scripts/vendor/popper.min.js"></script>
+        <script src="scripts/vendor/bootstrap.min.js"></script>
         <!-- Optional JavaScript -->
-
     </body>
 </html>
