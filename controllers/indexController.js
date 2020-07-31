2d1271db9a59aea3b801cb34e81b95bf4573c71a
auleki <kodagiwa@gmail.com>
2020-07-31 02:22:04 +0100

    refactor testing

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,9 @@
+const { readFile } = require('fs');
+
 exports.viewTest = (req, res) => {
     // res.render('index', { title: 'Express' });
-    res.send("Page Functional")
+    res.readFile("../index.html", "utf8", (err, data) => {
+        if(err) throw err;
+        res.send(data);
+    });
 }
